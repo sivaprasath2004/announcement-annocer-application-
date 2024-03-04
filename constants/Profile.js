5e91f4a8937ed67c70ca8301ab529a033d1dce4f
Sivaprasath <121082414+sivaprasath2004@users.noreply.github.com>
2024-03-04 14:10:30 +0530

    Update Profile.js

--- constants/Profile.js
+++ constants/Profile.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "ALLzaSyAbGbkFXsjSDAuXLVQTU7iFxNa-du74MbA",
-  authDomain: "initial-21c67.firebaseapp.com",
-  projectId: "initial-21c67",
-  storageBucket: "initial-21c67.appspot.com",
-  messagingSenderId: "908404905074",
-  appId: "1:908404905074:web:e0a7354b3e05ec644a1eeb",
-  measurementId: "G-Z12DLGMMQC"
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR DOMAIN",
+  projectId: "YOUR ID",
+  storageBucket: "YOUR BUCKET",
+  messagingSenderId: "YOUR SEDID",
+  appId: "YOUR APPID",
+  measurementId: "YOUR MESSID"
 };
 
 const app = initializeApp(firebaseConfig);
